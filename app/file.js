13bb6ec9d2942179546fdf92d9fcbf5d3d1358e2
Marco Vogt <marco.vogt@unibas.ch>
2024-11-07 21:14:13 +0100

    Host volume configuration

--- app/file.js
+++ app/file.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const filePath = path.join(__dirname, "posts.json");
+const filePath = path.join(__dirname, "data",  "posts.json");
 
 if (!fs.existsSync(filePath)) {
     fs.writeFileSync(filePath, JSON.stringify([]));
